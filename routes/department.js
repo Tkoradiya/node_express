2b27e47c9bf9d592604f2a08872ea47fad2979d4
Tkoradiya <koradiya.t@northeastern.edu>
2023-10-30 00:52:45 -0700

    latest codw

--- routes/department.js
+++ routes/department.js
@@ -1,26 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-var mysql = require('mysql');
+var sqlite3 = require('sqlite3'); 
 
-var connenction = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'db_test'
-});
-
-connenction.connect(function(err){
-  if(!err){
-    console.log("Department Database Connected");
-  }
-  else{
-    console.log("Error Connecting Database");
-  }
-});
+var connenction = new sqlite3.Database('./db/sqlite.db', sqlite3.OPEN_READWRITE, (err) => {
+  if(err) 
+  { 
+      console.log("Error Occurred - " + err.message); 
+  } 
+  else
+  { 
+      console.log("DataBase Connected"); 
+  } 
+}) 
 
 router.get('/add',function(req, res, next) {
+  console.log("123");
     res.render('department/department_add');
+    
   });
 
 router.post('/add',function(req, res){
@@ -28,14 +25,14 @@ router.post('/add',function(req, res){
     const mybodydata = {
       name : req.body.name
     }
-    connenction.query("insert into db_department set ?",mybodydata,function(err, result){
+    connenction.run("insert into department set ?",mybodydata,function(err, result){
       if(err) throw err;
       res.render('department/department_add',{ success : 'Data inserted' });
     })
 });
   
 router.get('/display', function(req, res, next) {
-    connenction.query("select * from db_department", function(err, db_rows){
+    connenction.run("select * from department", function(err, db_rows){
       if(err) throw err;
       console.log(db_rows);
       res.render('department/department_display', {db_rows_array: db_rows});
@@ -46,7 +43,7 @@ router.get('/delete/:id/', function(req,res,next){
     var deleteid = req.params.id;
     console.log("Delete is is:"+ deleteid);
   
-    connenction.query("Delete from db_department where department_id = ?", [deleteid], function(err,db_rows){
+    connenction.query("Delete from department where dept_id = ?", [deleteid], function(err,db_rows){
       if(err) throw err;
       console.log(db_rows);
       console.log("Record Deleted");
@@ -58,7 +55,7 @@ router.get('/show/:id/', function(req,res,next){
     var showid = req.params.id;
     console.log("Show id is:" + showid);
   
-    connenction.query("Select * from db_department where department_id = ?" , [showid], function(err, db_rows){
+    connenction.query("Select * from department where dept_id = ?" , [showid], function(err, db_rows){
       console.log(db_rows);
       if(err) throw err;
       res.render('department/department_show',{db_rows_array:db_rows});
@@ -67,9 +64,9 @@ router.get('/show/:id/', function(req,res,next){
   
 router.get('/edit/:id/',function(req,res,next){
   console.log("Edit id is:" + req.params.id);
-  var department_id = req.params.id;
+  var dept_id = req.params.id;
 
-  connenction.query("Select * from db_department where department_id = ?", [department_id], function(err, db_rows){
+  connenction.query("Select * from department where dept_id = ?", [dept_id], function(err, db_rows){
     if(err) throw error;
     console.log(db_rows);
     res.render('department/department_edit', {db_rows_array: db_rows});
@@ -83,7 +80,7 @@ router.post('/edit/:id/', function(req,res,next){
 
   var name = req.body.name;
 
-  connenction.query("Update db_department set name = ? where department_id = ?", [name, departmentid], function(err, respond){
+  connenction.query("Update department set dept_name = ? where dept_id = ?", [name, departmentid], function(err, respond){
     if(err) throw err;
     res.redirect('/department/display');
   })
--- routes/student.js
+++ routes/student.js
@@ -1,27 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-var mysql = require('mysql');
+var sqlite3 = require('sqlite3'); 
 
-var connenction = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'db_test'
-});
-
-connenction.connect(function(err){
-  if(!err){
-    console.log("Student Database Connected");
-  }
-  else{
-    console.log("Error Connecting Database");
-  }
-})
+var connenction = new sqlite3.Database('./db/sqlite.db', sqlite3.OPEN_READWRITE, (err) => {
+  if(err) 
+  { 
+      console.log("Error Occurred - " + err.message); 
+  } 
+  else
+  { 
+      console.log("DataBase Connected"); 
+  } 
+}) 
 
 router.get('/add',function(req, res, next) {
    
-  connenction.query("select * from db_department",function(err,db_rows){
+  connenction.run("select * from department",function(err,db_rows){
     if(err) throw err;
     console.log(db_rows);
     res.render('student/student_add',{db_rows_array:db_rows});
@@ -32,23 +27,22 @@ router.post('/add',function(req, res){
     console.log(req.body);
     const mybodydata = {
     
-      department_id : req.body.department_id,
-      first_name : req.body.first_name,
-      middle_name : req.body.middle_name,
-      last_name : req.body.last_name,
+      dept_id : req.body.dept_id,
+      fname : req.body.fname,
+      lname : req.body.lname,
       city : req.body.city,
       address : req.body.address,
       email : req.body.email,
       mobile : req.body.mobile
     }
-    connenction.query("insert into db_student set ?",mybodydata,function(err, result){
+    connenction.run("insert into student set ?",mybodydata,function(err, result){
       if(err) throw err;
       res.render('student/student_add',{ success : 'Data inserted' });
     })
 });
   
 router.get('/display', function(req, res, next) {
-    connenction.query("select * from db_student", function(err, db_rows){
+    connenction.query("select * from student", function(err, db_rows){
       if(err) throw err;
       console.log(db_rows);
       res.render('student/student_display', {db_rows_array: db_rows});
@@ -59,7 +53,7 @@ router.get('/delete/:id/', function(req,res,next){
     var deleteid = req.params.id;
     console.log("Delete is is:"+ deleteid);
   
-    connenction.query("Delete from db_student where student_id = ?", [deleteid], function(err,db_rows){
+    connenction.query("Delete from student where student_id = ?", [deleteid], function(err,db_rows){
       if(err) throw err;
       console.log(db_rows);
       console.log("Record Deleted");
@@ -71,7 +65,7 @@ router.get('/show/:id/', function(req,res,next){
     var showid = req.params.id;
     console.log("Show id is:" + showid);
   
-    connenction.query("Select * from db_student where student_id = ?" , [showid], function(err, db_rows){
+    connenction.query("Select * from student where student_id = ?" , [showid], function(err, db_rows){
       console.log(db_rows);
       if(err) throw err;
       res.render('student/student_show',{db_rows_array:db_rows});
@@ -82,11 +76,11 @@ router.get('/edit/:id/',function(req,res,next){
   console.log("Edit id is:" + req.params.id);
   var student_id = req.params.id;
 
-  connenction.query("Select * from db_student where student_id = ?", [student_id], function(err, db_rows){
+  connenction.query("Select * from student where student_id = ?", [student_id], function(err, db_rows){
     if(err) throw error;
     console.log(db_rows);
 
-    connenction.query("select * from db_student",function(err,db_rows){
+    connenction.query("select * from student",function(err,db_rows){
       if(err) throw err;
       console.log(db_rows);
     res.render('student/student_edit', {db_rows_array: db_rows});
@@ -99,16 +93,15 @@ router.post('/edit/:id/', function(req,res,next){
 
   var studentid = req.params.id;
 
-  var departmentid = req.body.department_id;
-  var firstname = req.body.first_name;
-  var middlename = req.body.middle_name;
-  var lastname = req.body.last_name;
+  var departmentid = req.body.dept_id;
+  var firstname = req.body.fname;
+  var lastname = req.body.lname;
   var city = req.body.city;
   var address = req.body.address;
   var email = req.body.email;
   var mobile = req.body.mobile;
 
-  connenction.query("Update db_student set department_id = ?, first_name = ?, middle_name = ?, last_name = ?, city = ?, address = ?, email = ?, mobile = ? where student_id = ?", [departmentid, firstname, middlename, lastname, city, address, email, mobile, studentid], function(err, respond){
+  connenction.query("Update student set dept_id = ?, fname = ?, lname = ?, city = ?, address = ?, email = ?, mobile = ? where student_id = ?", [departmentid, firstname, lastname, city, address, email, mobile, studentid], function(err, respond){
     if(err) throw err;
     res.redirect('/student/display');
   })
